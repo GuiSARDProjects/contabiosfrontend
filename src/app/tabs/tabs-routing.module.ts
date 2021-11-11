c7044c1b02f099fdf7ef64eb5ae62001e4697e72
Guilherme Lopes Pereira <gui-lopespereira@hotmail.com>
2021-11-10 23:56:21 -0300

    Início do desenvolvimento do simulador

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -12,8 +12,8 @@ const routes: Routes = [
         loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
       },
       {
-        path: 'tab2',
-        loadChildren: () => import('../simulador/simulador.module').then(m => m.Tab2PageModule)
+        path: 'simulador',
+        loadChildren: () => import('../simulador/simulador.module').then(m => m.SimuladorPageModule)
       },
       {
         path: 'tab3',
