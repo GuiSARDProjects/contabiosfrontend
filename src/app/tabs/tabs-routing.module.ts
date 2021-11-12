3b197e3bb4266a2fca8ad7794db40af3891ae5f8
Igor Sardinha <igor.sardinha@outlook.com>
2021-11-11 23:05:41 -0300

    modifield user tab

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -16,8 +16,8 @@ const routes: Routes = [
         loadChildren: () => import('../simulador/simulador.module').then(m => m.SimuladorPageModule)
       },
       {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+        path: 'user',
+        loadChildren: () => import('../user/user.module').then(m => m.UserPageModule)
       },
       {
         path: '',
